fc46f5b3cb0931d00647b6eb626e9ee8d907d8d3
Sumit Kumar Jha <jhasumit4742+github@gmail.com>
2024-06-07 16:56:04 +0530

    Enhancing Form input responsiveness

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -45,7 +45,7 @@ const ProfileForm = () => {
            
         }}
         >
-            <Form className='h-[90%] max-[400px]:w-[297px]   max-[400px]:bg-green-300'>
+            <Form className='h-[90%] max-[400px]:w-[297px]'>
                 <div id="formContainer" className='h-full p-4 flex max-[950px]:flex-col'>
                     <div id="dpWrapper" className='h-full basis-2/5 p-2 flex flex-col items-center'>
                         <div id="dpCircle" className='m-4 p-2 h-72 w-72 border-2 border-green-500 rounded-full flex items-center justify-center max-[400px]:w-48 max-[400px]:h-48'>
@@ -73,28 +73,28 @@ const ProfileForm = () => {
                         </div>
                         <ErrorMessage name="avatar" component="div" className="text-red-500 text-xl font-semibold" />
                     </div>
-                    <div id="formContainer" className='max-[400px]:m-0 m-4 basis-3/5 p-5'>
-                        <h2 className='font-bold text-2xl text-gray-800'>Profile Info</h2>
-                        <div id='inputField' className='max-[400px]:h-14 h-24 w-full p-2 flex items-center justify-center max-[570px]:gap-2'>
-                            <label htmlFor="firstName" className='basis-[30%] text-[#3B2B3F] font-semibold '>First Name</label>
+                    <div id="formContainer" className='max-[400px]:m-0 m-4 basis-3/5 p-5 max-[400px]:p-2 max-[400px]:flex max-[400px]:flex-col max-[400px]:gap-3 max-[400px]:bg-blue-300'>
+                        <h2 className='font-bold text-2xl text-gray-800 max-[400px]:text-center max-[400px]:mb-2'>Profile Info</h2>
+                        <div id='inputField' className='max-[400px]:h-14 h-24 w-full p-2 flex max-[400px]:flex-col items-center justify-center max-[570px]:gap-2 max-[400px]:gap-0'>
+                            <label htmlFor="firstName" className='basis-[30%] text-[#3B2B3F] font-semibold max-[400px]:items-start max-[400px]:w-full'>First Name</label>
                             <ErrorMessage name="firstName" component="div" className="text-red-500" />
-                            <Field type="text" name="firstName" id="firstName" className='basis-[70%] p-2 border border-gray-400 hover:border-[#3B2B3F] focus:shadow-sm focus:shadow-[#3B2B3F] focus:border-[#3B2B3F] focus:outline-none rounded-lg ' value={user.firstName} disabled/>
+                            <Field type="text" name="firstName" id="firstName" className='basis-[70%] p-2 border border-gray-400 hover:border-[#3B2B3F] focus:shadow-sm focus:shadow-[#3B2B3F] focus:border-[#3B2B3F] focus:outline-none rounded-lg' value={user.firstName} disabled/>
                         </div>
 
-                        <div id='inputField' className=' max-[400px]:h-14 h-24 w-full p-2 flex items-center justify-center max-[570px]:gap-2'>
-                            <label htmlFor="lastName" className='basis-[30%] text-[#3B2B3F] font-semibold '>Last Name</label>
+                        <div id='inputField' className=' max-[400px]:h-14 h-24 w-full p-2 flex max-[400px]:flex-col items-center justify-center max-[570px]:gap-2 max-[400px]:gap-0'>
+                            <label htmlFor="lastName" className='basis-[30%] text-[#3B2B3F] font-semibold max-[400px]:items-start max-[400px]:w-full '>Last Name</label>
                             <ErrorMessage name="lastName" component="div" className="text-red-500" />
                             <Field type="text" name="lastName" id="lastName" className='p-2 border border-gray-400 hover:border-[#3B2B3F] focus:shadow-sm focus:shadow-[#3B2B3F] focus:border-[#3B2B3F] focus:outline-none rounded-lg basis-[70%]' value={user.lastName} disabled/>
                         </div>
 
-                        <div id='inputField' className='max-[400px]:h-14 h-24 w-full p-2 flex items-center justify-center max-[570px]:gap-2'>
-                            <label htmlFor="role" className='basis-[30%] text-[#3B2B3F] font-semibold '>Role</label>
+                        <div id='inputField' className='max-[400px]:h-14 h-24 w-full p-2 flex max-[400px]:flex-col items-center justify-center max-[570px]:gap-2 max-[400px]:gap-0'>
+                            <label htmlFor="role" className='basis-[30%] text-[#3B2B3F] font-semibold max-[400px]:items-start max-[400px]:w-full '>Role</label>
                             <ErrorMessage name="role" component="div" className="text-red-500" />
                             <Field type="text" name="role" id="role" className='p-2 border border-gray-400 hover:border-[#3B2B3F] focus:shadow-sm focus:shadow-[#3B2B3F] focus:border-[#3B2B3F] focus:outline-none rounded-lg basis-[70%]' value={user.role} disabled/>
                         </div>
 
-                        <div id='inputField' className='max-[400px]:h-14 h-24 w-full p-2 flex items-center justify-center max-[570px]:gap-2'>
-                            <label htmlFor="role" className='basis-[30%] text-[#3B2B3F] font-semibold '>Email</label>
+                        <div id='inputField' className='max-[400px]:h-14 h-24 w-full p-2 flex max-[400px]:flex-col items-center justify-center max-[570px]:gap-2 max-[400px]:gap-0'>
+                            <label htmlFor="role" className='basis-[30%] text-[#3B2B3F] font-semibold max-[400px]:items-start max-[400px]:w-full '>Email</label>
                             <ErrorMessage name="email" component="div" className="text-red-500" />
                             <Field type="email" name="email" id="email" className='p-2 border border-gray-400 hover:border-[#3B2B3F] focus:shadow-sm focus:shadow-[#3B2B3F] focus:border-[#3B2B3F] focus:outline-none rounded-lg basis-[70%]' value={user.email} disabled />
                         </div>
