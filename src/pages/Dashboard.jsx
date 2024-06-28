15ba953c503117cd25fbd688fb1d8dee43213fa1
Sumit Kumar Jha <jhasumit4742+github@gmail.com>
2024-06-28 12:03:42 +0530

    Handled the ambiguity of token expiration

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,90 +1,130 @@
-import { Outlet, useNavigate} from 'react-router-dom'
-import Sidebar from '../components/Sidebar'
-import UserInfoBar from '../components/UserInfoBar'
-import { createContext, useEffect, useState } from 'react';
-import menu from "../assets/SVG/Menu.svg"
+import { Outlet, useNavigate } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import UserInfoBar from "../components/UserInfoBar";
+import { createContext, useEffect, useState } from "react";
+import menu from "../assets/SVG/Menu.svg";
 
 export const UserContext = createContext();
 
 const Dashboard = () => {
-  const navigate=useNavigate();
-  const [user, setUser] = useState('');
-  const token=localStorage.getItem("token");
+  const navigate = useNavigate();
+  const [user, setUser] = useState("");
+  const token = localStorage.getItem("token");
   const [isCollasp, setIsCollasp] = useState(false);
   const [buttonClick, setButtonClick] = useState(false);
-  const [screenwidth, setScreenwidth]=useState("")
-  
+  const [screenwidth, setScreenwidth] = useState("");
+
   useEffect(() => {
-    const fetchUser = async(e)=>{
-        try {
-          console.log("Token:",token)
-          const response=await fetch("https://osteoai-backend.onrender.com/api/user",{
-            headers:{
-              "Authorization": `Bearer ${token}`,
+    const fetchUser = async (e) => {
+      try {
+        console.log("Token:", token);
+        const response = await fetch(
+          "https://osteoai-backend.onrender.com/api/user",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
             },
-          });
-          
-          const result=await response.json();
-          setUser(result)
-        } catch (error) {
-          console.error(error.message);
+          }
+        );
+
+        if (!response.ok || response.status === 401) {
+          localStorage.removeItem("token");
+          navigate("/login");
+          return;
         }
-      }  
-      if(token){
-        fetchUser();
-       
-      }else{
-        navigate("/login")
+        const result = await response.json();
+        setUser(result);
+      } catch (error) {
+        console.error(error.message);
       }
-    }, [navigate, token])
-    
-    useEffect(()=>{
-
-      const screenSize=()=>{
-        setScreenwidth(window.innerWidth)
-      }
-      window.addEventListener('resize', screenSize)
-      screenSize();
-
-      if(screenwidth<=1200){
-        setIsCollasp(true)
-      }
-      return ()=> window.removeEventListener('resize', screenSize)
-    },[])
-
-    const handleCollasp =()=>{
-        setIsCollasp(!isCollasp)
-        setButtonClick(true)
+    };
+    if (token) {
+      fetchUser();
+    } else {
+      navigate("/login");
     }
-    
-    return (
-    <>
-          <div id="dashboardWrapper" className='h-screen w-full flex'>
-              <div id="sidebarContainer" className={(screenwidth<=1200 && isCollasp===true) ? 'hidden':'basis-[15%] bg-white h-screen rounded-r-2xl p-1 border-y-2 border-r-2 border-r-gray-300 hover:shadow-lg max-[400px]:h-[810px] '}>
-                  <Sidebar screenwidth={screenwidth} setIsCollasp={setIsCollasp} setButtonClick={setButtonClick}/>
-              </div>
-              {buttonClick==true ? <></> :
-              <button onClick={handleCollasp} className={screenwidth>=1200 ? 'hidden' : 'h-8 w-8 mt-2 shrink-0'}>
-                <img src={menu} alt="menu" className='sm:shrink-0'/>
-              </button>
-              }
-              <div id="contentAreaContainer" className={(screenwidth<=1200 || isCollasp===true) ? 'basis-[100%] p-1':' basis-[85%] p-1'}>
-                <div id="contentcontainer" className={screenwidth<=1100 ? 'h-full flex flex-col' : 'h-full flex'}>
-                  <div id="mainContentContainer" className='max[1100px]:basis-[90%] max-[660px]:basis-[95%] max-[530px]:basis-[95%] basis-[80%] min-[1100px]:overflow-y-auto max-[400px]:w-[320px]'>
-                    <UserContext.Provider value={{user, setUser}}>
-                      <Outlet/>
-                    </UserContext.Provider>
-                  </div>
-                  <div id="userInfoContainer" className='max-[1100px]:basis-[10%] max-[660px]:basis-[5%] max-[530px]:basis-[5%] max-[400px]:w-[328px] basis-[20%]'>
-                    <UserContext.Provider value={{user, setUser}}>
-                      <UserInfoBar screenwidth={screenwidth}/>
-                    </UserContext.Provider>
-                  </div>
-                </div>
-              </div>
-          </div>
-    </>
-  )
-}
+  }, [navigate, token]);
 
-export default Dashboard
+  useEffect(() => {
+    const screenSize = () => {
+      setScreenwidth(window.innerWidth);
+    };
+    window.addEventListener("resize", screenSize);
+    screenSize();
+
+    if (screenwidth <= 1200) {
+      setIsCollasp(true);
+    }
+    return () => window.removeEventListener("resize", screenSize);
+  }, []);
+
+  const handleCollasp = () => {
+    setIsCollasp(!isCollasp);
+    setButtonClick(true);
+  };
+
+  return (
+    <>
+      <div id="dashboardWrapper" className="h-screen w-full flex">
+        <div
+          id="sidebarContainer"
+          className={
+            screenwidth <= 1200 && isCollasp === true
+              ? "hidden"
+              : "basis-[15%] bg-white h-screen rounded-r-2xl p-1 border-y-2 border-r-2 border-r-gray-300 hover:shadow-lg max-[400px]:h-[810px] "
+          }
+        >
+          <Sidebar
+            screenwidth={screenwidth}
+            setIsCollasp={setIsCollasp}
+            setButtonClick={setButtonClick}
+          />
+        </div>
+        {buttonClick == true ? (
+          <></>
+        ) : (
+          <button
+            onClick={handleCollasp}
+            className={screenwidth >= 1200 ? "hidden" : "h-8 w-8 mt-2 shrink-0"}
+          >
+            <img src={menu} alt="menu" className="sm:shrink-0" />
+          </button>
+        )}
+        <div
+          id="contentAreaContainer"
+          className={
+            screenwidth <= 1200 || isCollasp === true
+              ? "basis-[100%] p-1"
+              : " basis-[85%] p-1"
+          }
+        >
+          <div
+            id="contentcontainer"
+            className={
+              screenwidth <= 1100 ? "h-full flex flex-col" : "h-full flex"
+            }
+          >
+            <div
+              id="mainContentContainer"
+              className="max[1100px]:basis-[90%] max-[660px]:basis-[95%] max-[530px]:basis-[95%] basis-[80%] min-[1100px]:overflow-y-auto max-[400px]:w-[320px]"
+            >
+              <UserContext.Provider value={{ user, setUser }}>
+                <Outlet />
+              </UserContext.Provider>
+            </div>
+            <div
+              id="userInfoContainer"
+              className="max-[1100px]:basis-[10%] max-[660px]:basis-[5%] max-[530px]:basis-[5%] max-[400px]:w-[328px] basis-[20%]"
+            >
+              <UserContext.Provider value={{ user, setUser }}>
+                <UserInfoBar screenwidth={screenwidth} />
+              </UserContext.Provider>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
