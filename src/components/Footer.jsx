51db63f63dea3d0b3dd6021448d0fdcfb57e4f42
Sumit Kumar Jha <jhasumit4742@gmail.com>
2025-03-17 22:23:49 +0530

    Added Github link at footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import github from "../assets/SVG/Github.svg";
 
 const Footer = () => {
   return (
-    <footer className="container mt-10 flex flex-col items-center py-10 ">
+    <footer className="container mt-10 flex flex-col md:flex-row items-center justify-center py-10 gap-2">
       <p className="cursor-pointer text-center text-md font-semibold font-mono">
-        © 2024 OsteoAI. All rights reserved.
+        © 2024 OsteoAI. All rights reserved
       </p>
+      <button className="h-10 w-auto text-white gap-1 text-lg font-mono bg-gray-800 flex items-center p-2 rounded-lg hover:bg-gray-900">
+        <a href="https://github.com/sumitkrjha/OsteoAI" target="_blank">
+          <img src={github} alt="OsteoAI Github" className="h-5 w-7" />
+        </a>
+        OsteoAI
+      </button>
     </footer>
   );
 };
